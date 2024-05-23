88af4aaee13967b49b7bbfc6771bb555acb3730b
Dev-Farhana <code.nana22@gmail.com>
2024-05-23 21:08:36 +0500

    Second commit

---
+++ src/components/Main/MainComp.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import "./Main.css";
+import { assets } from '../../assets/assets';
+
+const Main = () => {
+  return (
+    <div className='main'>
+      <div className="nav">
+        <p> Gemini </p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p> <span> Hello! Dev </span>  </p>
+          <p> How can i help you today? </p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest Besutiful places </p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          
+          <div className="card">
+            <p> Brainstorm team bonding activites for our retreat </p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+
+          <div className="card">
+            <p>Improve the readability of following icon. </p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+
+        </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text"
+             placeholder='Enter Prompt here.'
+             />
+             <div>
+              <img src={assets.gallery_icon} alt="" />
+              <img src={assets.mic_icon} alt="" />
+              <img src={assets.send_icon} alt="" />
+             </div>
+          </div>
+          <p className='bottom-info'> 
+          Gemini may display inaccurate info, including about people, so double check the responses. Your privacy and Gemini App.
+          </p>
+
+        </div>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Main;
