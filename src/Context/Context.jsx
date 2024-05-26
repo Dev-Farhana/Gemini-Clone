be45eb30764d904b6855cea9ca40e8ad61392ca6
Dev-Farhana <code.nana22@gmail.com>
2024-05-26 15:08:03 +0500

    Second commit

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,12 +1,40 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
+import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
+    const [ input, setInput] = useState("");
+    const [ recentPrompt, setRecentPrompt] = useState("");
+    const [ prevPrompts, setPrevPrompts ] = useState([]);
+    const [ showResult, setShowResult ] = useState(false);
+    const [ loading, setLoading ] = useState(false);
+    const [ resultData, setResultData ] = useState("");
+
+    const onSent = async(prompt) => {
+        setLoading(true);
+        setResultData("");
+        setShowResult(true);
+       const response =  await runChat(input);
+       setResultData(response);
+       setLoading(false)
+       setInput("");
+       
+    }
 
     const contextValue = {
-
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        recentPrompt,
+        setRecentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
     }
+
     return (
         <Context.Provider value={contextValue}>
         {props.children}
--- src/components/Main/MainComp.jsx
+++ src/components/Main/MainComp.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import "./Main.css";
 import { assets } from '../../assets/assets';
+import { useContext } from 'react';
+import { Context } from '../../Context/Context';
 
 const Main = () => {
+    const { prevPrompts, setPrevPrompts,
+      recentPrompt, setRecentPrompt,
+      showResult, loading,
+      onSent,  resultData,
+      input, setInput,
+    } = useContext(Context) ;
+
   return (
     <div className='main'>
       <div className="nav">
@@ -34,13 +43,18 @@ const Main = () => {
         <div className="main-bottom">
           <div className="search-box">
             <input type="text"
+             value={input}
+             onChange={(e) => setInput(e.target.value) }
              placeholder='Enter Prompt here.'
-             />
-             <div>
+            />
+            <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" />
-             </div>
+              <img 
+               src={assets.send_icon} alt="" 
+               onClick={()=> onSent()  } 
+              />
+            </div>
           </div>
           <p className='bottom-info'> 
           Gemini may display inaccurate info, including about people, so double check the responses. Your privacy and Gemini App.
